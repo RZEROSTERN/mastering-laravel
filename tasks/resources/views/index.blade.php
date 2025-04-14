fee0b1a92c7e3542cca5dfdf8d3c0ba8eb04d606
Marco Ramírez <marco.ramirez@rzerocorp.com>
2025-04-14 17:06:34 -0600

    Initial commit. Task organizator project started.

---
+++ tasks/resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <h1>Hello, butt-nuggets</h1>
+</div>
+<div>
+    @forelse($tasks as $task)
+        <div>
+            <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{$task->title}}</a>
+        </div>
+    @empty
+        <div>There are no tasks</div>
+    @endforelse
+</div>
